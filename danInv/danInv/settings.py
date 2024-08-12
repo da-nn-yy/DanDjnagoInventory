dbf99677b6f13519eeb1d4eee976678a1f3d8c7f
da-nn-yy <db7394928@gmail.com>
2024-08-12 23:30:07 +0200

    makesd migration and updated pip installs

--- danInv/danInv/settings.py
+++ danInv/danInv/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'danInvApp',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLET_PACK = 'bootstrap5'
